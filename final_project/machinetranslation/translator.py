bc6e54194ffb935729455a62b2520a6e83f1b34c
ericel <ericel123new@gmail.com>
2023-04-18 22:29:30 -0400

    Funtions for translating text added

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -3,6 +3,7 @@ from ibm_watson import LanguageTranslatorV3
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 import os
 from dotenv import load_dotenv
+from ibm_watson import ApiException
 
 load_dotenv()
 
@@ -10,22 +11,37 @@ APIKEY = os.environ['apikey']
 URL = os.environ['url']
 VERSION = "2018-05-01"
 
-# Create an instance of the IBM Watson Language translator
-authenticator = IAMAuthenticator('{APIKEY}')
-language_translator = LanguageTranslatorV3(
-    version='{VERSION}',
-    authenticator=authenticator
-)
+try:
+    # Create an instance of the IBM Watson Language translator
+    authenticator = IAMAuthenticator(APIKEY)
+    language_translator = LanguageTranslatorV3(
+        version=VERSION,
+        authenticator=authenticator
+    )
 
-language_translator.set_service_url('{URL}')
+    language_translator.set_service_url(URL)
 
-language_translator.set_disable_ssl_verification(True)
-
+    language_translator.set_disable_ssl_verification(True)
+    
+except ApiException as ex:
+    print("Method failed with status code " + str(ex.code) + ": " + ex.message)
 
 def englishToFrench(englishText):
-    #write the code here
+    """
+    This function translates text from English to French
+    """
+    translation = language_translator.translate(
+    text=englishText,
+    model_id='en-fr').get_result()
+    frenchText = translation["translations"][0]['translation']
     return frenchText
 
 def frenchToEnglish(frenchText):
-    #write the code here
+    """
+    This function translates text from French to English
+    """
+    translation = language_translator.translate(
+    text=frenchText,
+    model_id='fr-en').get_result()
+    englishText = translation["translations"][0]['translation']
     return englishText
