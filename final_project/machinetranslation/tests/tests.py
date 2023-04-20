9547a82b9dbf79108ab378369b7c52c28c61f688
ericel <ericel123new@gmail.com>
2023-04-19 21:31:14 -0400

    final changes

--- final_project/machinetranslation/tests/tests.py
+++ final_project/machinetranslation/tests/tests.py
@@ -1,13 +1,13 @@
 import unittest
-from machinetranslation.translator import french_to_english, english_to_french
+from translator import french_to_english, english_to_french
 class TestFrenchToEnglish(unittest.TestCase): 
     def test1(self): 
-        self.assertIsNone(french_to_english(None)) # test when Null is entered, Should return None.
+        self.assertNotEqual(french_to_english(None), '') # test when Null is entered, Should return None.
         self.assertEqual(french_to_english("Bonjour"), "Hello")  # test when "Bonjour" is entered should return "Hello"
         
 class TestEnglishToFrench(unittest.TestCase): 
     def test1(self): 
-        self.assertIsNone(english_to_french(None)) # test when Null is entered, Should None.
+        self.assertNotEqual(english_to_french(None), '') # test when Null is entered, Should None.
         self.assertEqual(english_to_french("Hello"), "Bonjour")  # test when "Hello" is entered should return "Bonjour"    
 
 unittest.main()
